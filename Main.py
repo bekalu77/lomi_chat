b83b9fe0f7e2a719d7672437dd8f742dbfe26609
bekalu77 <106968778+bekalu77@users.noreply.github.com>
2025-03-21 06:29:46 +0300

    Main.py

--- Main.py
+++ Main.py
@@ -9,6 +9,7 @@ import sqlite3
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_GROUP_ID = int(os.getenv("ADMIN_GROUP_ID"))
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
 RENDER_URL = os.getenv("RENDER_URL")  # e.g., https://your-service-name.onrender.com
 
 bot = telebot.TeleBot(BOT_TOKEN)
