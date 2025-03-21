8245302921beb9c650669947e2db5d78f053521d
bekalu77 <106968778+bekalu77@users.noreply.github.com>
2025-03-21 12:19:01 +0300

    Main.py

--- Main.py
+++ Main.py
@@ -65,6 +65,7 @@ with DatabaseConnection() as cursor:
         user_id INTEGER PRIMARY KEY,
         category TEXT,
         last_activity REAL DEFAULT (strftime('%s', 'now'))
+)
     """)
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS posts (
